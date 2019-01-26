642d65c8653a3beb0da7728c9256f26610341c27
Barracuda72 <barracuda72@bk.ru>
2019-01-26 21:49:54 +0500

    Add header guard

--- include/decl/size_t.h
+++ include/decl/size_t.h
@@ -7,9 +7,9 @@
  *
  */
 
-#ifndef _SIZE_T_H
-#define _SIZE_T_H 1
+#ifndef _DECL_SIZE_T_H
+#define _DECL_SIZE_T_H 1
 
 typedef unsigned long  size_t;
 
-#endif // _SIZE_T_H
+#endif // _DECL_SIZE_T_H
