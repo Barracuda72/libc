c44ad6aeed9e90acdd03382084997db99235ec2f
Barracuda72 <barracuda72@bk.ru>
2019-01-26 21:51:08 +0500

    wchar_t.h moved to separate header

---
+++ include/decl/wchar_t.h
@@ -0,0 +1,15 @@
+/*
+ * wchar_t.h
+ *
+ * Отдельный заголовочный файл
+ *
+ * Определение типа wchar_t
+ *
+ */
+
+#ifndef _DECL_WCHAR_T_H
+#define _DECL_WCHAR_T_H 1
+
+typedef unsigned short wchar_t;
+
+#endif // _DECL_WCHAR_T_H
