2b8a384bfe10b4db5f0266aec679ca6d6d0bc959
Barracuda72 <barracuda72@bk.ru>
2020-05-24 22:04:55 +0500

    Oops, forgot to commit this one

--- stdio/int/xscanf.c
+++ stdio/int/xscanf.c
@@ -1,16 +1,16 @@
 /*
- * int/xprintf.c
+ * int/xscanf.c
  *
  * Internal functions
  *
- * Generic *printf function
+ * Generic *scanf function
  *
  */
 
 #include <stdio.h>
 #include <int/stdio.h>
 
-int int_xprintf(struct int_stdio_desc* stream, const char* restrict format, va_list args)
+int int_xscanf(struct int_stdio_desc* stream, const char* restrict format, va_list args)
 {
   // TODO
   return 0;
